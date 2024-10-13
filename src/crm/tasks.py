960daa17d037042e0a60a419de1312fab7651676
gutz <01iVeR@inbox.ru>
2024-10-13 20:17:50 +0500

    add product and change it

--- src/crm/tasks.py
+++ src/crm/tasks.py
@@ -1,9 +1,9 @@
-from celery import Celery
-
-
-celery_app = Celery('tasks', broker='redis://localhost')
-
-
-@celery_app.task
-def ret_hello(username):
-    print(username)
+# from celery import Celery
+#
+#
+# celery_app = Celery('tasks', broker='redis://localhost')
+#
+#
+# @celery_app.task
+# def ret_hello(username):
+#     print(username)
--- src/crm/tests.py
+++ src/crm/tests.py
@@ -1,22 +1,22 @@
-from functools import wraps
-
-
-def inner_dec(arg):
-    def dec(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            print('before', arg)
-            res = func(*args, **kwargs)
-            print(res)
-            print('after', arg-1)
-        return wrapper
-    return dec
-
-@inner_dec(1)
-def print_hello(title):
-    return f'hello {title}'
-
-# know = inner_dec('hello')(print_hello('oleg'))
-# funcs = know.__name__
-# print(funcs)
-print_hello('oleg')
+# from functools import wraps
+#
+#
+# def inner_dec(arg):
+#     def dec(func):
+#         @wraps(func)
+#         def wrapper(*args, **kwargs):
+#             print('before', arg)
+#             res = func(*args, **kwargs)
+#             print(res)
+#             print('after', arg-1)
+#         return wrapper
+#     return dec
+#
+# @inner_dec(1)
+# def print_hello(title):
+#     return f'hello {title}'
+#
+# # know = inner_dec('hello')(print_hello('oleg'))
+# # funcs = know.__name__
+# # print(funcs)
+# print_hello('oleg')
